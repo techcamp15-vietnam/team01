ea9e01c69f6d068233c2cc76da93e80fd1cac830
hajimemashoo <dinhvantiep2009@gmail.com>
2014-01-07 18:32:17 +0700

    Add file ResultScreen.java. Activity ResultScreen display result screen of game.

---
+++ TechCampTeam01/src/com/example/techcampteam01/ResultScreen.java
@@ -0,0 +1,22 @@
+package com.example.techcampteam01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Display result screen of game.
+ * 
+ * @author TiepDV
+ * 
+ */
+
+public class ResultScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.result_layout);
+		super.onCreate(savedInstanceState);
+	}
+
+}
