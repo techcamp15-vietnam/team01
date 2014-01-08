c16ba23604a21003c32c5b862b2760f3b9200922
hajimemashoo <dinhvantiep2009@gmail.com>
2014-01-08 23:09:22 +0700

    Add file Setting.java

---
+++ TechCampTeam01/src/com/example/techcampteam01/Setting.java
@@ -0,0 +1,17 @@
+package com.example.techcampteam01;
+
+public class Setting {
+
+	public static boolean soundOn = true;
+	public static boolean musicOn = true;
+
+	public static void setSound(boolean isOn) {
+
+		soundOn = isOn;
+	}
+
+	public static void setMusic(boolean isOn) {
+		musicOn = isOn;
+	}
+
+}
