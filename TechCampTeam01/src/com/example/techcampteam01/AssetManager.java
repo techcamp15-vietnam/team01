25d88351ecc0323253f4f2bd652828228fe14bd0
hajimemashoo <dinhvantiep2009@gmail.com>
2014-01-08 10:03:37 +0700

    Add file AssetManager. File manage Resource of game.

---
+++ TechCampTeam01/src/com/example/techcampteam01/AssetManager.java
@@ -0,0 +1,41 @@
+package com.example.techcampteam01;
+
+/***
+ * @author　ティエップ
+ * Manage resource
+ */
+import android.graphics.Bitmap;
+
+public class AssetManager {
+
+	public static Bitmap rawbitmap;
+
+	public static Bitmap splashRaw;
+
+	MainActivity context;
+
+	public AssetManager(MainActivity context) {
+
+		this.context = context;
+
+	}
+
+	/**
+	 * @author ティエップ Load Resoucre.
+	 */
+
+	public void load() {
+
+	}
+
+	/**
+	 * Dispose resource when don't uses
+	 * 
+	 * @author ティエップ
+	 */
+
+	public void disposeResoure() {
+
+	}
+
+}
