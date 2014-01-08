f260444d0dc5464394b16e411771b106f9d41de4
tungtobe <tung.tobe@gmail.com>
2014-01-08 18:54:28 +0700

    add Share Image to Facebook

--- TechCampTeam01/src/com/example/techcampteam01/ResultScreen.java
+++ TechCampTeam01/src/com/example/techcampteam01/ResultScreen.java
@@ -1,121 +1,59 @@
 package com.example.techcampteam01;
 
+import java.io.File;
+
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphObject;
-import com.facebook.model.OpenGraphAction;
-import com.facebook.model.OpenGraphObject;
-import com.facebook.widget.FacebookDialog;
-
-/**
- * Display result screen of game.
- * 
- * @author ティエプ
- * 
- */
+import android.widget.Toast;
 
 public class ResultScreen extends FragmentActivity {
-	private UiLifecycleHelper uiHelper;
-	private static final String TAG = "Your App";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		setContentView(R.layout.result_layout);
 		super.onCreate(savedInstanceState);
-		uiHelper = new UiLifecycleHelper(this, callback);
-		uiHelper.onCreate(savedInstanceState);
 
 		Button btnShare = (Button) findViewById(R.id.btn_share);
 		btnShare.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				pushstory();
-
+				shareImage("abc");
 			}
 		});
 	}
 
-	private void pushstory() {
-		OpenGraphObject meal = OpenGraphObject.Factory
-				.createForPost("cooking-app:meal");
-		meal.setProperty("title", "Buffalo Tacos");
-		meal.setProperty("image",
-				"http://example.com/cooking-app/images/buffalo-tacos.png");
-		meal.setProperty("url",
-				"https://example.com/cooking-app/meal/Buffalo-Tacos.html");
-		meal.setProperty("description", "Leaner than beef and great flavor.");
+	/**
+	 * Post Result Image to FB
+	 * 
+	 * @param imageName
+	 * 
+	 * @author 1-C トゥン
+	 */
 
-		OpenGraphAction action = GraphObject.Factory
-				.create(OpenGraphAction.class);
-		action.setProperty("meal", meal);
+	private void shareImage(String imgName) {
 
-		FacebookDialog shareDialog = new FacebookDialog.OpenGraphActionDialogBuilder(
-				this, action, "cooking-app:cook", "meal").build();
-		// FacebookDialog shareDialog = new
-		// FacebookDialog.OpenGraphActionDialogBuilder(
-		// this, action, "meal").build();
-		uiHelper.trackPendingDialogCall(shareDialog.present());
+		Intent share = new Intent(Intent.ACTION_SEND);
+
+		share.setType("image/png");
+
+		String imagePath = Environment.getExternalStorageDirectory() + "/"
+				+ imgName + ".png";
+		Toast.makeText(getBaseContext(), imagePath, Toast.LENGTH_SHORT).show();
+
+		File imageFileToShare = new File(imagePath);
+
+		Uri uri = Uri.fromFile(imageFileToShare);
+		share.putExtra(Intent.EXTRA_STREAM, uri);
+
+		startActivity(Intent.createChooser(share, "Share Image!"));
 	}
 
-	private Session.StatusCallback callback = new Session.StatusCallback() {
-		@Override
-		public void call(Session session, SessionState state,
-				Exception exception) {
-			onSessionStateChange(session, state, exception);
-		}
-	};
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		Session session = Session.getActiveSession();
-		if (session != null && (session.isOpened() || session.isClosed())) {
-			onSessionStateChange(session, session.getState(), null);
-		}
-		uiHelper.onResume();
-	}
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		uiHelper.onActivityResult(requestCode, resultCode, data);
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		uiHelper.onPause();
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		uiHelper.onSaveInstanceState(outState);
-	}
-
-	private void onSessionStateChange(Session session, SessionState state,
-			Exception exception) {
-		if (state.isOpened()) {
-			Log.i(TAG, "Logged in...");
-		} else if (state.isClosed()) {
-			Log.i(TAG, "Logged out...");
-		}
-	}
 }
